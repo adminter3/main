b81c16f33109516dac201abf686502f2f1aed35f
adminster3 <adminster3@hotmail.com>
2017-06-12 14:55:55 +0200

    Prepeare for the weather api calls

---
+++ weather_api_calls.js
@@ -0,0 +1,5 @@
+
+var wearther=new XMLHttpRequest();
+wearther.open("GET","http://api.wunderground.com/api/fa5715ba5adff2ac/conditions/q/CA/San_Francisco.json",false)
+wearther.send(null);
+https://www.youtube.com/watch?v=53AoDB7vcbU
