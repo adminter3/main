af2884684aad7f21edbd2064fa0ad33789f88797
adminster3 <adminster3@hotmail.com>
2017-08-21 10:51:53 +0200

    added chart paint

--- jsCode.js
+++ jsCode.js
@@ -1,7 +1,8 @@
-var kakan=new XMLHttpRequest();       kakan.open("GET","http://api.fredagskakan.se/veckanskaka",false);
+var kakan = new XMLHttpRequest();
+kakan.open("GET","http://api.fredagskakan.se/veckanskaka",false);
 kakan.send(null);
 
-var r =JSON.parse(kakan.response);
+var r = JSON.parse(kakan.response);
 var date=r.date;
 var kaknamn=r.kaka;
 var pic=r.pic;
@@ -12,3 +13,5 @@ pic_.appendChild(img);
 
 document.getElementById("date").innerHTML=date;
 document.getElementById("kaka").innerHTML=kaknamn;
+
+
